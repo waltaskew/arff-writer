df9f5b5f5452c01029398d9b2f1b54b27d4d8c39
waltaskew <waltaskew@gmail.com>
2013-03-08 15:40:57 -0600

    first pass at corpus reader

---
+++ src/author_reader.py
@@ -0,0 +1,23 @@
+"""Grab texts and authors from a corpus directory."""
+
+
+import os
+
+
+def find_authors(corpus_dir):
+    """Given a corpus directory, find the set of authors by looking at
+    its sub-directories.
+    """
+    return [name for name in os.listdir(corpus_dir) if
+            os.path.isdir(os.path.join(corpus_dir, name))]
+
+
+def iter_text(corpus_dir):
+    """Return a generator yielding a text and its author for all
+    entries in corpus_dir.
+    """
+    for author in find_authors(corpus_dir):
+        author_dir = os.path.join(corpus_dir, author)
+        for text in os.listdir(author_dir):
+            with open(text, 'r') as text_file:
+                yield text_file.read(), author
