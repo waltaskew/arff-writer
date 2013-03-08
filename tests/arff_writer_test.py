f0a16f79a4de185c21208354c26ec69fec2ca7aa
waltaskew <waltaskew@gmail.com>
2013-03-08 16:16:52 -0600

    remove redundant attribute_string tests

--- tests/arff_writer_test.py
+++ tests/arff_writer_test.py
@@ -24,44 +24,12 @@ class FeatureFunctionTest(unittest.TestCase):
 
         feature_func = arff_writer.FeatureFunction(
                 lambda x: x,
-                attribute_name='snakes_moo',
-                attribute_type='integer')
-        self.assertEqual(
-                feature_func.attribute_string(),
-                '@ATTRIBUTE snakes_moo integer')
-
-        feature_func = arff_writer.FeatureFunction(
-                lambda x: x,
-                attribute_name='snakes_hiss',
-                attribute_type='real')
-        self.assertEqual(
-                feature_func.attribute_string(),
-                '@ATTRIBUTE snakes_hiss real')
-
-        feature_func = arff_writer.FeatureFunction(
-                lambda x: x,
                 attribute_name='snakes_sleep',
                 attribute_type='nominal',
                 nominals=['boo', 'bah', '"beep"'])
         self.assertEqual(
                 feature_func.attribute_string(),
                 r'@ATTRIBUTE snakes_sleep {"boo","bah","\"beep\""}')
-
-        feature_func = arff_writer.FeatureFunction(
-                lambda x: x,
-                attribute_name='snakes_explode',
-                attribute_type='string')
-        self.assertEqual(
-                feature_func.attribute_string(),
-                '@ATTRIBUTE snakes_explode string')
-
-        feature_func = arff_writer.FeatureFunction(
-                lambda x: x,
-                attribute_name='snakes_bloop',
-                attribute_type='date')
-        self.assertEqual(
-                feature_func.attribute_string(),
-                '@ATTRIBUTE snakes_bloop date')
 
     def test_set_type_from_val(self):
         """Test the set_type_from_val method."""
